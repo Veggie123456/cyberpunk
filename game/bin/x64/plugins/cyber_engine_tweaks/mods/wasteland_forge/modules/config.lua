4b6bdfe07f17de0917edd7428b38eda725b0ee9d
Veggie123456 <153017368+Veggie123456@users.noreply.github.com>
2026-09-18 16:09:59 -0400

    Expand Forge systems and feature architecture

---
+++ game/bin/x64/plugins/cyber_engine_tweaks/mods/wasteland_forge/modules/config.lua
@@ -0,0 +1,9 @@
+local Config = {
+  gridSnap = 0.25,
+  rotationSnap = 5.0,
+  placementDistance = 4.0,
+  autosave = true,
+  maxUndo = 50,
+  categories = {"Structures","Furniture","Utility","Lighting","Decor","Barriers"}
+}
+return Config
