3ee1b5bc50cf9f14d1c2acb965747f0b8f89ccba
Veggie123456 <153017368+Veggie123456@users.noreply.github.com>
2026-09-18 16:07:09 -0400

    Scaffold Wasteland Forge v0.1

---
+++ game/bin/x64/plugins/cyber_engine_tweaks/mods/wasteland_forge/modules/assets.lua
@@ -0,0 +1,9 @@
+local Assets={}
+Assets.all={
+  {label="Concrete Barrier",path="PLACEHOLDER:barrier"},
+  {label="Cargo Container",path="PLACEHOLDER:container"},
+  {label="Work Light",path="PLACEHOLDER:light"},
+  {label="Chair",path="PLACEHOLDER:chair"},
+  {label="Table",path="PLACEHOLDER:table"}
+}
+return Assets
